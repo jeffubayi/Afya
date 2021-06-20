66673226537009ba5fb218cfccaa72aefff7aaf1
jeffubayi <jeffubayi@gmail.com>
2021-06-20 13:37:09 +0300

    themes - add the custom theme colors used Globally within the app
    uses material ui createTheme function to inject the themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+//styles imports to inject the main theme off the app
+import { theme } from "./css/globalStyles";
+import { ThemeProvider } from "@material-ui/core/styles";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        analytics
-      </header>
+    <div>
+      <ThemeProvider theme={theme}>
+     
+    </ThemeProvider>
     </div>
   );
 }
---
+++ src/css/globalStyles.tsx
@@ -0,0 +1,14 @@
+//Global theme styles throughout the app
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  // Color themes for the entire app
+  palette: {
+    primary: {
+      main: "#4615b2",
+    },
+    secondary: {
+      main: "#707070",
+    },
+  },
+});
