3db6833d63294ae17beb9de2b83a05e5f484c6c2
jeffubayi <jeffubayi@gmail.com>
2021-06-21 03:29:06 +0300

    add styles for the main navigation menu components

--- src/css/globalStyles.tsx
+++ src/css/globalStyles.tsx
@@ -3,12 +3,20 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   // Color themes for the entire app
+  overrides: {
+    // you declare you override default material-ui styles
+    MuiDrawer: {
+      paper: {
+        paddingTop: "14rem",
+      },
+    },
+  },
   palette: {
     primary: {
       main: "#4615b2",
     },
     secondary: {
-      main: "#707070",
+      main: "#FFFFF",
     },
   },
 });
