14726b551619eb3e37c0e3c503c01cf79e2fe9a4
jeffubayi <jeffubayi@gmail.com>
2021-06-22 23:23:49 +0300

    analytics - create  the analytics page
    acts as the wrapper for all  analytics components

--- src/screens/analytics/index.tsx
+++ src/screens/analytics/index.tsx
@@ -1,22 +1,69 @@
 import React from "react";
-import { Drawer, AppBar, CssBaseline, List } from "@material-ui/core";
+import {
+  Drawer,
+  AppBar,
+  CssBaseline,
+  List,
+  Paper,
+  Grid,
+} from "@material-ui/core";
 import { appBarItems, drawerItems } from "../../components/menuItems";
+//components
+import Card from "./components/MetricsCard";
+import VisitList from "./components/VisitList";
+import StaffList from "./components/StaffList";
+import LabelTabs from "./components/LabelTabs";
+import KeyIssues from "./components/KeyIssues";
+//styles
+import styles from "../../css/styles";
 
 export default function index() {
   return (
-    <div>
+    <div style={styles.body}>
       <CssBaseline />
       <AppBar position="absolute" color="secondary" elevation={0}>
         {appBarItems}
       </AppBar>
-
       <Drawer variant="permanent">
         <List>{drawerItems}</List>
       </Drawer>
-
-      <main>
+      <div>
         <h1>Analytics</h1>
-      </main>
+        <Grid container spacing={1}>
+          <Grid container xs={4} spacing={1}>
+            <Grid item xs={4}>
+              <Paper style={styles.visits}>
+                <VisitList />
+              </Paper>
+            </Grid>
+          </Grid>
+
+          <Grid container xs={8} spacing={3}>
+            <Grid item xs={12}>
+              <Paper style={styles.layout}>
+                <KeyIssues />
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper style={styles.layout}>
+                <LabelTabs />
+              </Paper>
+            </Grid>
+            <Grid item xs={4}>
+              <Card title="Footfall" amount={4.8} caption="patients" />
+            </Grid>
+            <Grid item xs={4}>
+              <Card title="Patient Satisfaction" amount={13} caption="NPS" />
+            </Grid>
+            <Grid item xs={4}>
+              <Card title="Revenue" amount={4.2} caption="kenyan shillings" />
+            </Grid>
+            <Grid item xs={12}>
+              <StaffList />
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
