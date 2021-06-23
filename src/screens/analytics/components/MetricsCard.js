e6ba0f9e146fcc8fd149151ce876d31833f7374e
jeffubayi <jeffubayi@gmail.com>
2021-06-23 12:44:57 +0300

    cards - update the card ui

--- src/screens/analytics/components/MetricsCard.js
+++ src/screens/analytics/components/MetricsCard.js
@@ -1,33 +1,32 @@
 import { Box, Card, CardContent, Grid, Typography } from "@material-ui/core";
-import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
-import line from "../../../css/assests/graph.png";
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDropDown';
 import styles from "../../../css/styles";
 import PropTypes from 'prop-types'
 
 
-const MetricsCard = ({title,caption,amount}) => (
+const MetricsCard = ({title,caption,amount,percentage,graph}) => (
   <Card style={styles.area}>
     <CardContent>
       <Grid container spacing={3} style={styles.grid}>
         <Grid item>
-          <Typography color="textSecondary" gutterBottom variant="h6">
+          <Typography gutterBottom variant="subtitle1">
             {title}
           </Typography>
-          <Typography color="primary" variant="subtitle1">
+          <Typography color="primary" style={styles.textOne}>
             {amount}
           </Typography>
         </Grid>
         <Grid item>
-          <img src={line} alt="graph" style={styles.img} />
+          <img src={graph} alt="graph" style={styles.img} />
         </Grid>
       </Grid>
 
-      <Typography color="textSecondary" variant="body2">
+      <Typography color="textSecondary" style={styles.textTwo}>
         {caption}
       </Typography>
       <Box style={styles.patients}>
-        <ArrowDownwardIcon style={styles.iconRed} />
-        <Typography>12%</Typography>
+        <ArrowDownwardIcon style={styles.iconGreen} />
+        <Typography style={styles.textTwo}>{percentage}%</Typography>
       </Box>
     </CardContent>
   </Card>
@@ -36,5 +35,7 @@ MetricsCard.propTypes = {
   title: PropTypes.string.isRequired,
   caption: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
+  percentage: PropTypes.number.isRequired,
+  graph: PropTypes.string.isRequired,
 };
 export default MetricsCard;
