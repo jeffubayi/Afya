829601b286013591f3d205e66a30c6d2df66baf8
jeffubayi <jeffubayi@gmail.com>
2021-06-29 14:55:23 +0300

    revert changes on key issues

--- src/screens/analytics/components/KeyIssues.tsx
+++ src/screens/analytics/components/KeyIssues.tsx
@@ -27,13 +27,7 @@ const Item = () => {
   );
 };
 
-const KeyIssues: React.FC = () => {
-  const { data, loading, error } = useQuery(GET_STAFF);
-
-  if (loading) return <p>loading...</p>;
-  if (error) return <p>ERROR</p>;
-  if (!data) return <p>Not found</p>;
-  const rows = data.getAllStaff;
+export default function KeyIssues() {
   return (
     <div>
       <Typography
