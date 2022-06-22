cf8115bd7ae1151091b66c37836c33b7e7b7f5f2
jeffubayi <jeffubayi@gmail.com>
2022-06-22 09:30:50 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const colors = require('colors');
-const cors = require('cors');
-require('dotenv').config();
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
-const connectDB = require('./config/db');
-// Accessing the path module
-const path = require("path");
-const port = process.env.PORT || 5000;
+// const express = require('express');
+// const colors = require('colors');
+// const cors = require('cors');
+// require('dotenv').config();
+// const { graphqlHTTP } = require('express-graphql');
+// const schema = require('./schema/schema');
+// const connectDB = require('./config/db');
+// // Accessing the path module
+// const path = require("path");
+// const port = process.env.PORT || 5000;
 
-const app = express();
+// const app = express();
 
-// Connect to database
-connectDB();
+// // Connect to database
+// connectDB();
 
-app.use(cors());
+// app.use(cors());
 
 // app.use(
 //   '/graphql',
@@ -24,10 +24,57 @@ app.use(cors());
 //   })
 // );
 
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
+
+// app.listen(port, console.log(`Server running on port ${port}`));
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+//import your models
+require("./models/quote");
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//import routes
+require("./routes/quoteRoute.js")(app);
+
+const PORT = process.env.PORT || 5000;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-app.listen(port, console.log(`Server running on port ${port}`));
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
+
