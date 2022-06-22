232f9a6b79e2800c40055d6c05fac065146471f6
jeffubayi <jeffubayi@gmail.com>
2022-06-22 15:08:20 +0300

    deploy

--- config/db.js
+++ config/db.js
@@ -1,9 +1,27 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+const DATABASE_URL = process.env.MONGO_URI;
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+// const connectDB = async () => {
+//   const conn = await mongoose.connect(process.env.MONGO_URI);
+
+//   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+// };
+
+const connectDB = () => {
+  return mongoose.connect(DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
+    if (err) {
+      console.log("Connection to Database failed.");
+    }
+    else{
+      console.log("Database connection successful.");
+    }
+  });
 };
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 module.exports = connectDB;
