274239ebc90e804312048157ac40a6ced00f1f11
jeffubayi <jeffubayi@gmail.com>
2022-06-22 09:16:32 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
+// Accessing the path module
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -14,13 +16,18 @@ connectDB();
 
 app.use(cors());
 
-app.use(
-  path.join(express.static(path__dirname, "client", "build"))
-  
-);
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema,
+//     graphiql: process.env.NODE_ENV === 'development',
+//   })
+// );
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(port, console.log(`Server running on port ${port}`));
